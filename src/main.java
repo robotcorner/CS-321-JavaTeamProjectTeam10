b0a69076b28aafeba0cac7c366759479fe432845
Stephen Stammen <sms0066@uah.edu>
2022-01-10 20:31:12 +0600

    Initial file setup for git and java team project this semester

---
+++ src/main.java
@@ -0,0 +1,12 @@
+// This is a default file for the sake of starting version control.
+// The main file would use the class structure definitions as needed.
+
+/*
+Course:         CS-321
+Project:        Build an app with GUI in swing for managing a book or movie watch later personal list.
+Requirements:
+    Use version control.
+    Have 3-4 members.
+
+Authors:    Stephen Stammen, _____, _____, ____
+ */
