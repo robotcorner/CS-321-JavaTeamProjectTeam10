d3a4c2610e65691fc08f09d1d6691cd1437037e5
robotcorner <sms0066@uah.edu>
2022-03-28 14:20:02 -0500

    Implemented Movie Class. Relates #22

--- src/Movie.java
+++ src/Movie.java
@@ -2,6 +2,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+/**
+ * This is a Movie Object Template
+ *      Each constructor will be populated from the JSON file.
+ *      We don't need setters for everything because we expect the contructor
+ *          to do that.
+ */
 public class Movie {
 
     // Instance Variables
@@ -9,34 +16,109 @@ public class Movie {
     private String title;
     private int year;
     private String rated;
-    private Date released;
-    private int runtime;
+    private Date dateReleased;
+    private String runtime;
     private String genre;
     private String director;
     private String actors;
-    private String language;
+    private String languages;
     private String poster;
     private int metascore;
     private float imdbRating;
     private String imdbVotes;
     private String typeOfContent;
 
-    Movie(String title, String year, String rated, String released, String runtime, String genre, String director, String actors, String language, String poster, String metascore, String imdbRating, String imdbVotes, String imdbID, String typeOfContent) throws ParseException {
-        this.imdbID = imdbID;
+    Movie(String title, String year, String rated, String released, String runtime, String genre, String director, String actors, String languages, String poster, String metascore, String imdbRating, String imdbVotes, String imdbID, String typeOfContent) throws ParseException {
         this.title = title;
         this.year = Integer.parseInt(year);
         this.rated = rated;
-        this.released = new SimpleDateFormat("dd MMM yyyy").parse(released);
+        this.genre = genre;
+        this.runtime = runtime;
+        this.dateReleased = new SimpleDateFormat("dd MMM yyyy").parse(released);
+        this.director = director;
+        this.actors = actors;
+        this.languages = languages;
+        this.poster = poster;
+        this.metascore = Integer.parseInt(metascore);
+        this.imdbRating = Float.parseFloat(imdbRating);
+        this.imdbVotes = imdbVotes;
+        this.typeOfContent = typeOfContent;
+        this.imdbID = imdbID;
     }
 
-    public static void printMovieToConsole(Movie media) {
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public Date getDateReleased() {
+        return dateReleased;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public String getLanguage() {
+        return languages;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public int getMetascore() {
+        return metascore;
+    }
+
+    public float getImdbRating() {
+        return imdbRating;
+    }
+
+    public String getImdbVotes() {
+        return imdbVotes;
+    }
+
+    public String getTypeOfContent() {
+        return typeOfContent;
+    }
+
+    /**
+     * Prints out the movie to the console
+     * @param media
+     */
+    public static void printSimpleMovieToConsole(Movie media) {
         // This is independent of the instances and tests a single movie object passed in.
         System.out.println("-----------------------------------------------------------------------------");
         System.out.println("Title | IMDbID | Year | Rated | Released");
-        System.out.printf("%s | %s | %d | %s | %tc", media.title, media.imdbID, media.year, media.rated, media.released);
+        System.out.printf("%s | %s | %d | %s | %tc", media.title, media.imdbID, media.year, media.rated, media.dateReleased);
         System.out.println();
         System.out.println("-----------------------------------------------------------------------------");
     }
+
 }
 
 
--- src/Tests.java
+++ src/Tests.java
@@ -2,10 +2,22 @@ import java.text.ParseException;
 
 class Tests {
     static void testMovie() throws ParseException {
-        // Create a movie from JSON...
-        // TODO: Use the JSON Class to get the formated version to input in the future.
+
+        // TODO: TEST: PROPERLY IMPORT MOVIE INTO MOVIE OBJECT FROM JSON FILE
+
+
+        // TEST: MOVIE CONSTRUCTOR
         Movie movie1 = new Movie("Dune", "2021", "PG-13", "22 Oct 2021", "155 min", "Action, Adventure, Drama", "Denis Villeneuve", "Timothée Chalamet, Rebecca Ferguson, Zendaya", "English, Mandarin", "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg", "74", "8.1","444,702", "tt1160419", "movie");
-        Movie.printMovieToConsole(movie1);
+
+        // TEST: MOVIE PRINT TO CONSOLE
+        Movie.printSimpleMovieToConsole(movie1);
+
+        // TEST: MOVIE GETTER FUNCTIONS
+        System.out.println("Movie Title: " + movie1.getTitle() + "|  Movie Genre: " + movie1.getYear());
+
+        // TEST: MOVIE GETTER FUNCTION - STRING WITH COMMAS
+        System.out.println(movie1.getGenre());
+
     }
 }
 
