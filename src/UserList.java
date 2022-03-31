8a024a20d8eafe9df8d2b2de7c1084184412c4bc
CalebBagwell <rcb0027@uah.edu>
2022-03-30 22:49:12 -0500

    Update UserList.java

--- src/UserList.java
+++ src/UserList.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class UserList {
 
-    private ArrayList<String> userList;
+    private ArrayList<User> userList;
 
     public void addUser(String u, String p) {
 
-        
+        User user = new User(u, p);
+
+        userList.add(user);
     }
 }
