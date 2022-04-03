e05b21041d57f50bfcea4d186b3fb0cebf3d1a83
Stephen <sms0066@uah.edu>
2022-04-03 17:32:54 -0500

    created opening / main view (MovieManagerView) and made it work with UserView

--- src/MovieManagerView.java
+++ src/MovieManagerView.java
@@ -1,20 +1,65 @@
 import javax.swing.*;
 
+
 public class MovieManagerView {
-    private JPanel moviepanel;
-    private JComboBox comboBox1;
-    private JTextField textField1;
+    static UserView accountView;
 
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Movie View");
-        frame.setSize(1000,1000);
-        frame.setContentPane(new MovieManagerView().moviepanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    public MovieManagerView(UserView userView) {
+        this.accountView = userView;
+        System.out.println("initialized account view");
     }
 
+
+    public static void main() {
+        JFrame frame = new JFrame("Main");
+        JPanel frameP = new JPanel();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,1000);
+
+        // Build out the TopBar
+        // The SEARCH bar for the top middle section
+        JTextField searchBar = new JTextField("Search Here");
+        searchBar.setVisible(true);
+        searchBar.setLocation(frame.getWidth()/2, 20);
+        searchBar.setVisible(true);
+
+
+        JPanel loginSection = new JPanel();
+        loginSection.add(new JLabel("Login/Signup: "));
+        JButton loginBtn = new JButton("Login");
+        loginBtn.addActionListener(e-> {
+            System.out.println("Pressed Login Button");
+            accountView.openLogInView();
+        });
+        JButton signupBtn = new JButton("Sign-up");
+        signupBtn.addActionListener(e-> {
+            System.out.println("Pressed Sign-Up Button");
+            accountView.openSignUpView();
+        });
+
+        loginSection.add(loginBtn);
+        loginSection.add(signupBtn);
+        loginSection.setLocation((frame.getWidth()-60), 20);
+        loginSection.setVisible(true);
+
+
+        JPanel topBar = new JPanel();
+        topBar.add(searchBar);
+        topBar.add(loginSection);
+        topBar.setVisible(true);
+        JPanel moviePanel = new JPanel();
+
+        // add the two main pieces
+        frameP.add(topBar);
+        frameP.add(moviePanel);
+        frameP.setVisible(true);
+        //frame.pack();
+        frame.setContentPane(frameP);
+        // frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
 
 }
